94de0371cbbdd5f53e4c3511f2ec8d88903d0088
HiyaWorld <alesha.kapoor.8@gmail.com>
2026-02-10 14:28:29 +0000

    Create PrintGreetings.c

---
+++ PrintGreetings.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("I am Alesha.\n");
+    sleep(1); 
+    printf("My Bhaiya is the best!\n");
+    sleep(1);  
+    printf("My Mummy and Daddy are the best too!\n");
+    sleep(1); 
+    printf("Let's get a pet asap!");
+}

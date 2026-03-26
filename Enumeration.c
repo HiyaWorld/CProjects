6f737ffa76e8b9479a19d5ae5ba4bb6b10694d72
HiyaWorld <alesha.kapoor.8@gmail.com>
2026-03-26 17:20:59 +0000

    Create Enumeration.c

---
+++ Enumeration.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+enum specialwords{
+    sanjay = 11,
+    tanu ,
+    nilesh = 23,
+    alesha ,
+    low,
+    medium,
+    high
+};
+
+int main() 
+{
+
+    printf("the value is %d\n",medium);
+    return 0;
+}

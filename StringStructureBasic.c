8784c74bb682273dbda6d84e58c65085c2f45593
HiyaWorld <alesha.kapoor.8@gmail.com>
2026-04-27 20:22:51 +0100

    Create StringStructureBasic.c

---
+++ StringStructureBasic.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+
+    char fn[] = "Sanjay";
+    char sn[] = "Kapoor";
+    char fullname[20];
+    fullname[0] = fn[0];
+    fullname[1] = fn[1];
+    fullname[2] = fn[2];
+    fullname[3] = fn[3];
+    fullname[4] = fn[4];
+    fullname[5] = fn[5];
+    fullname[6] = ' ';
+    fullname[7] = sn[0];
+    fullname[8] = sn[1];
+    fullname[9] = sn[2];
+    fullname[10] = sn[3];
+    fullname[11] = sn[4];
+    fullname[12] = sn[5];
+    printf("%s\n", fullname);
+}

4a1c3520f6b113cab2f46b9476876614908a4f09
HiyaWorld <alesha.kapoor.8@gmail.com>
2026-04-28 16:00:54 +0100

    Create Enumeration2.c

---
+++ Enumeration2.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+//enumeration
+typedef enum EatingHabit
+{
+    Carnivorous = 1,
+    Herbivorous,
+    Omnivorous
+} EatingHabit;
+
+int main() {
+    // Write C code here
+    
+    EatingHabit LionsHabit = Carnivorous;
+    
+}
